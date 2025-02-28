ad3cebb6a71418c5d07e05edbfc48bb08b6e28a9
Ujjwal Singh <121544271+helloujjwal@users.noreply.github.com>
2025-03-01 00:47:53 +0530

    Revert "added readme and updated folder structure and removed unwanted test f…"

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,4 @@
-import { PlaywrightTestConfig } from '@playwright/test';
-import path from 'path';
-import type { ExcelConfig } from './utils/excelJS_utils';
-// import { readUrlsFromExcel, saveDataToExcel } from '../utils/excelJS_utils';
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -11,59 +8,74 @@ import type { ExcelConfig } from './utils/excelJS_utils';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// Excel configuration
-const excelConfig: ExcelConfig = {
-  defaultExcelPath: path.join(process.cwd(), 'testdata'),
-  sheets: {
-    urls: 'urls.xlsx',
-    results: 'test-results.xlsx',
-    data: 'test-data.xlsx'
-  }
-};
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-const config: PlaywrightTestConfig = {
-  testDir: './workflows',
-  timeout: 600000000,
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['html', { outputFolder: 'playwright-report' }]
-  ],
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: process.env.ENV === 'prod' ? 'https://prod-url.com' : 'https://stage-url.com',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    trace: 'retain-on-failure',
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    headless: false,
+
   },
+
+  /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'Chromium',
-    //   use: {
-    //     browserName: 'chromium',
-    //     viewport: { width: 1920, height: 1080 },
-    //     launchOptions: {
-    //       args: ['--start-maximized']
-    //     }
-    //   },
-    // },
     {
-      name: 'Firefox',
-      use: {
-        browserName: 'firefox',
-        viewport: { width: 1920, height: 1080 },
-      },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
     // {
-    //   name: 'Webkit',
-    //   use: {
-    //     browserName: 'webkit',
-    //     viewport: { width: 1920, height: 1080 },
-    //   },
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-};
 
-export { config as default, excelConfig };
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
